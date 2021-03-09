0417979e6647b6e5002033ec0fcbf9f50c593b08
mrudhvika940 <mrudhvikadamaraju@gmail.com>
2021-03-09 18:24:18 +0530

    Updated simulation

--- experiment/simulation/js/main.js
+++ experiment/simulation/js/main.js
@@ -1,1 +1,8 @@
-//Your JavaScript goes in here
+function getOption(lang){
+	if(lang=="null")
+	{
+		alert("Select language");
+		return;
+	}
+	$('#words_sentence').load('CLexp2.php?lang='+lang+'&words_selected=%&words=%&possible_sent=%&turn=0&position=%');
+}
